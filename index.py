88a438100f85cc53f52ba957b83490ad972c41be
valieree <124511726+valieree@users.noreply.github.com>
2023-12-20 14:31:58 +0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,37 @@
+import ydb
+
+from employees_reader import reader
+from dbclient import ydb_client
+from serialize import to_json_array
+from exception import ConnectionFailure
+
+import json
+
+pool = ydb.SessionPool(ydb_client.create_driver())
+
+def bad_request(code: int = 400, body = None):
+  return { 
+    'statusCode': code,
+    'body': body
+  }
+
+def usual_responce(result):
+  return { 
+    'statusCode': 200,
+    'body': to_json_array(result[0].rows)
+  }
+
+def employees_get_request(parameters):
+  if parameters['data'] == 'automobile_list':
+    return usual_responce(pool.retry_operation_sync(reader.serial_numbers_with_models))
+  else:
+    return bad_request('Incorrect query parameters')
+
+def handler(event, context):
+  if (event['httpMethod'] == 'GET'):
+    return employees_get_request(event['queryStringParameters'])
+  else:
+    return bad_request()
+  return{
+    'statusCode': 200
+}
