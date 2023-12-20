a78b49009132e3720a4e912b234f79a41c718bb7
valieree <124511726+valieree@users.noreply.github.com>
2023-12-20 14:38:44 +0300

    Create client.js

---
+++ client.js
@@ -0,0 +1,20 @@
+// Конструктор, формирующий поля класса по объекту JSON
+class Employee {
+    constructor(obj) {
+        this.EMPLid = obj.EMPLid;
+        this.name = obj.name;
+        this.client = obj.client;
+    }
+    print() {
+        console.log(this.to_string());
+    }
+    to_string() {
+        return "Id: " + this.id + ", name: " + this.NAME + ", client: " + this.client;
+    }
+    to_table_entry() {
+        return '<tr><td>' +
+            this.EMPLid + '</td><td>' +
+            this.name + '</td><td>' +
+            this.client + '</td><td>';
+    }
+};
